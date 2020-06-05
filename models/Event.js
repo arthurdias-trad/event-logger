22e48d644e8c2863efc45967487247479e52f7cf
Arthur Dias <arthurdias.trad@outlook.com>
2020-06-05 13:51:35 -0300

    Created Event MongoDB model

---
+++ models/Event.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add the event name"],
+    trim: true,
+  },
+  client: {
+    type: String,
+    trim: true,
+  },
+  field: {
+    type: String,
+    required: [true, "Please add the field of the event"],
+    trim: true,
+  },
+  subfield: {
+    type: String,
+    trim: true,
+  },
+  startDate: {
+    type: Date,
+    required: [true, "Enter the start date of the event"],
+  },
+  endDate: {
+    type: Date,
+  },
+  comments: {
+    type: String,
+    trim: true,
+  },
+});
+
+EventSchema.set("collection", "events");
+
+EventSchema.pre("save", function (next) {
+  if (!this.endDate) {
+    this.endDate = this.startDate;
+  }
+  next();
+});
+
+module.exports = mongoose.model("Event", EventSchema);
